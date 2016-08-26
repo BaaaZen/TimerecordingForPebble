94b3a37461303faf2887a4f47db7229897f86696
BaaaZen <baaazen@gmail.com>
2016-08-27 00:36:42 +0200

    WIP: JS-Interface zum Setzen von Timeline-Pins erweitert

--- pebble/src/js/pebble-js-app.js
+++ pebble/src/js/pebble-js-app.js
@@ -1,5 +1,7 @@
-const C_JS_CMD_ADD_PIN = 1;
+const C_JS_CMD_SET_PIN = 1;
 const C_JS_CMD_DEL_PIN = 2;
+
+const C_JS_PIN_TYPE_FINISH_TIME = 1;
 
 var timeline = require('./timeline');
 var timelineToken = null;
@@ -18,12 +20,34 @@ Pebble.addEventListener('appmessage', function(msg) {
   console.log('AppMessage received: ' + JSON.stringify(msg));
   if(msg.payload['JS_KEY_CMD']) {
     // this message is for the js interface
-    if(msg.payload['JS_KEY_CMD'] == C_JS_CMD_ADD_PIN) {
-      var pin = {
-        'id': msg.payload['JS_KEY_TLPIN_ID']
-      }
-    } else if(msg.payload['JS_KEY_CMD'] == C_JS_CMD_DEL_PIN) {
+    if(msg.payload['JS_KEY_CMD'] == C_JS_CMD_SET_PIN) {
+      // all necessary data present?
+      if(msg.payload['JS_KEY_TLPIN_ID'] && msg.payload['JS_KEY_TLPIN_TIME'] && msg.payload['JS_KEY_TLPIN_NAME'] && msg.payload['JS_KEY_TLPIN_TYPE']) {
+        // layout
+        var layout = {}
+        layout['type'] = 'genericPin';
+        layout['title'] = msg.payload['JS_KEY_TLPIN_NAME'];
+        layout['lastUpdated'] = new Date().toISOString();
+        if(msg.payload['JS_KEY_TLPIN_TYPE'] == -1 /* this need to be replaced for more than one type! */) {
+          /* fill me */
+        } else {
+          layout['tinyIcon'] = 'system://images/RESULT_SENT';
+        }
 
+        var pin = {};
+        pin['id'] = msg.payload['JS_KEY_TLPIN_ID'];
+        pin['time'] = msg.payload['JS_KEY_TLPIN_TIME'];
+        pin['layout'] = layout;
+        /* TODO: set reminder */
+        timeline.insertUserPin(pin, function(responseText) {
+          console.log('Inserting pin via Web-API results in: ' + responseText);
+        });
+      } else {
+        console.log('Incomplete SET_PIN message!');
+      }
+
+    } else if(msg.payload['JS_KEY_CMD'] == C_JS_CMD_DEL_PIN) {
+      /* TODO */
     }
 
   }
